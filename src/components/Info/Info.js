0cd6c99a473965e33daac95867e04334d6439335
Edgar Alcolea <edgaralcolea@gmail.com>
2022-02-02 20:52:56 +0100

    Created Context and Provider and created toggleCallStatus

--- src/components/Info/Info.js
+++ src/components/Info/Info.js
@@ -1,4 +1,8 @@
-const Info = ({ callStatus }) => {
+import { useContext } from "react";
+import Provider from "../Context/ContextProvider";
+
+const Info = () => {
+  const { callStatus } = useContext(Provider);
   return <span className={`message ${callStatus}`}>Calling...</span>;
 };
 export default Info;
